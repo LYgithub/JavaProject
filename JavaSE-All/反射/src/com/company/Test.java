315ac881e1801b2eb52ea8ecdd64357df0764d3b
liyang <YangKaw@126.com/KawYang@163.com>
2020-10-12 17:39:56 +0800

    反射

---
+++ JavaSE-All/反射/src/com/company/Test.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import org.w3c.dom.Text;
+
+/**
+ * @author LiYang
+ * @Project Name: 反射
+ * @Package Name: com.company
+ * Created by MacBook Air on 2020/10/12.
+ * Copyright © 2020 LiYang. All rights reserved.
+ */
+public class Test {
+    public int id;
+    private String name;
+
+    public Test(){
+        id = 1;
+        name = "yang";
+    }
+
+    public Test(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void print(){
+        System.out.println(this.id + "==>" + this.name);
+    }
+}
